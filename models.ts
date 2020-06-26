0964582efb5066a5af77cc44ae313f79fc4d4242
Ian Lord <lordi@ezmax.ca>
2020-06-26 04:58:18 -0400

    Preparing release 1.0.0

--- models.ts
+++ models.ts
@@ -240,6 +240,10 @@ export interface EzsigndocumentRequest {
    * Represent a Date Time. The timezone is the one configured in the User\'s profile.
    */
   dtEzsigndocumentDuedate: string;
+  /**
+   * The ID of the language, Valid values are: 1. French 2. English
+   */
+  fkiLanguageID: number;
 }
 
 /**
@@ -333,10 +337,6 @@ export interface EzsignfolderRequest {
    * The Ezsign Folder Type ID. This value can be queried by the API and is also visible in the admin interface.  There are two types of Ezsignfoldertype. **User** and **Shared**. **User** can only be seen by the user who created the folder or its assistants. Access to **Shared** folders are configurable for access and email delivery. You should typically choose a **Shared** type here.
    */
   fkiEzsignfoldertypeID: number;
-  /**
-   * The ID of the language, Valid values are: 1. French 2. English
-   */
-  fkiLanguageID: number;
   /**
    * Determine if a Time Stamping Authority should add a timestamp on each of the signature. Valid values are: 1. No. No TSA Timestamping will requested. This will make all signatures a lot faster since no round-trip to the TSA server will be required. Timestamping will be made using eZsign server\'s time. 2. Best effort. Timestamping from a Time Stamping Authority will be requested but is not mandatory. In the very improbable case it cannot be completed, the timestamping will be made using eZsign server\'s time. **Additional fee applies** 3. Mandatory. Timestamping from a Time Stamping Authority will be requested and is mandatory. In the very improbable case it cannot be completed, the signature will fail and the user will be asked to retry. **Additional fee applies** 
    */
