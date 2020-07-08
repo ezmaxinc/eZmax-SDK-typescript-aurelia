d5d5709504b979a7bcbd55b4749ab3ace797c2a6
Ian Lord <lordi@ezmax.ca>
2020-07-08 10:25:33 -0400

    Fix Cell type

--- models.ts
+++ models.ts
@@ -526,6 +526,6 @@ export interface EzsignsignerRequestCompoundContact {
   /**
    * The Cell Phone number of the contact. Use format \"5149901516\" for North American Numbers (Without \"1\" for long distance code) you would dial like this: 1-514-990-1516. Use format \"498945233886\" for international numbers (Without \"011\") you would dial like this: +49 89 452 33 88-6. In this example \"49\" is the country code of Germany.
    */
-  sPhoneNumberCell?: any;
+  sPhoneNumberCell?: string;
 }
 
